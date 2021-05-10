51a7b2a7abb42f7e779c8bfe219462a55f45c349
jensrott <jens.rottiers@scarlet.be>
2021-05-10 15:20:38 +0200

    Added exercises

---
+++ frontend_mentor/exercise_7/rest-countries-api-with-color-theme-switcher-master/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
